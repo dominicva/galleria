0ff3c5172184b6e96a54d7ec595179d1bad0ffce
Dominic van Almsick <dominicvana@gmail.com>
2023-05-23 14:48:04 +0100

    feat: init homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import logo from './assets/galleria.svg';
+import starryNightSmall from './assets/starry-night/hero-small.jpg';
 
 function App() {
   return (
     <>
       <header>
-        <h1>
-          <img src={logo} alt="Galleria logo" />
-        </h1>
+        <img src={logo} alt="Galleria logo" />
+
         <button>start slideshow</button>
       </header>
+      <section className="gallery">
+        <figure>
+          <img src={starryNightSmall} alt="Starry Night" />
+          <figcaption>
+            <h2>Starry Night</h2>
+            <h3 className="subhead-2">Vincent Van Gogh</h3>
+          </figcaption>
+        </figure>
+      </section>
     </>
   );
 }
