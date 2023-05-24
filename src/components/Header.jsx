ab62018c7ce3ab0b1c3e33c273b74b52b9089128
Dominic van Almsick <dominicvana@gmail.com>
2023-05-24 14:26:47 +0100

    feat: link to gallery from details

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
+import { Link } from 'react-router-dom';
 import logo from '/public/assets/galleria.svg';
 
 export default function Header() {
   return (
     <header>
       <div>
-        <img src={logo} alt="Galleria logo" />
-        <h1 className="sr-only">galleria.</h1>
+        <Link to="/">
+          <img src={logo} alt="Galleria logo" />
+          <h1 className="sr-only">galleria.</h1>
+        </Link>
       </div>
       <button>start slideshow</button>
     </header>
