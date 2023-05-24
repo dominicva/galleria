3719c9f3b405a57fe2d9336853596f520c04745e
Dominic van Almsick <dominicvana@gmail.com>
2023-05-24 14:17:32 +0100

    feat: add gradient to gallery image

--- src/components/PaintingPreview.jsx
+++ src/components/PaintingPreview.jsx
@@ -4,7 +4,10 @@ export default function PaintingPreview({ name, artist, imageSrc }) {
   return (
     <Link to={`gallery/${name}`}>
       <figure>
-        <img src={imageSrc} alt={name} />
+        <div className="gradient-container">
+          <div className="gradient-background"></div>
+          <img src={imageSrc} alt={name} />
+        </div>
         <figcaption>
           <h2>{name}</h2>
           <h3 className="subhead-2">{artist}</h3>
